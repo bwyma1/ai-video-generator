00a75c4c6a52641d893f17655b6fca22631366ed
bwyma1 <brockdwyma@gmail.com>
2023-11-07 13:25:00 -0600

    Generating movie transcript. Generating audio

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from video_generator import VideoGenerator
 from sys import argv
+import whisper_timestamped as whisper
+
 
 tiktok_generator = VideoGenerator(argv[1])
-tiktok_generator.generate_video()
+tiktok_generator.generate_video(argv[2])
--- video_generator.py
+++ video_generator.py
@@ -1,11 +1,13 @@
 from moviepy.editor import *
-from PIL import Image
+import whisper_timestamped as whisper
 
 class VideoGenerator(object):
     def __init__(self,movie):
         """ Initialize with a movie (must be .mp4) """
         self.movie = movie
-        self.audio = movie.audio
+        self.vfc_movie = VideoFileClip(movie)
+        self.text_w_timestamps = []
+        self.transcript = ''
 
     def _splice_movie(self, timestamps):
         """ Using the timestamps, the function will splice together
@@ -20,14 +22,47 @@ class VideoGenerator(object):
     def _splice_audio(self, timestamps):
         """ Using the timestamps, the function will splice together
             the audio clips. Timestamps is a list of lists."""
+        audio = self.vfc_movie.audio
         if(len(timestamps) == 0):
             raise ValueError("Timestamps can't be empty")
         clips = []
         for i in range(0, len(timestamps)):
-            clips.append(self.audio.subclip(timestamps[i][0], timestamps[i][1]))
+            clips.append(audio.subclip(timestamps[i][0], timestamps[i][1]))
         return concatenate_audioclips(clips)
     
-    def generate_video(self):
+    def _generate_text_w_timestamps(self):
+        """ Using the whisper_timestamped package from 
+            https://github.com/linto-ai/whisper-timestamped the 
+            function will create a full timestamped transcript of the movie."""
+        audio = whisper.load_audio(self.movie)
+        model = whisper.load_model("base")
+
+        result = whisper.transcribe(model, audio, language="en")
+
+        for segment in result['segments']:
+            for word in segment['words']:
+                self.transcript += ' ' + word['text']
+                self.text_w_timestamps.append([word['text'],word['start']])
+
+    def _generate_phrase_audio(self, phrases):
+        """ Given phrases (list of string), the function will find and return an 
+            AudioFileClip object containing the audio for that phrases """
+        timestamps = []
+        for phrase in phrases:
+            phrase = phrase.split()
+            for i in range(0,len(self.text_w_timestamps)-len(phrase)):
+                matching = True
+                for j in range(0,len(phrase)):
+                    if(self.text_w_timestamps[i+j][0] != phrase[j]):
+                        matching = False
+                if(matching):
+                    timestamps.append(self._splice_audio(
+                        [self.text_w_timestamps[i][1],
+                         self.text_w_timestamps[i+len(phrase)-1][1] ]))
+                    break
+        return self._splice_audio(timestamps)
+
+    def generate_video(self, outputFile):
         """ Generates an amazing, emotional TikTok short video  using AI
             technology to find the most emotional and aesthetic scenes"""
                          
